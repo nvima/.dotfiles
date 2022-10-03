253e46aef36aeba27816f2ccd819884b1c0474b0
jens3911 <jens3911@live.de>
2022-10-03 18:45:03 +0200

    added tsserver path bec of some random errors

--- nvim/.config/nvim/lua/user/lsp/init.lua
+++ nvim/.config/nvim/lua/user/lsp/init.lua
@@ -7,6 +7,14 @@ require "user.lsp.configs"
 require("user.lsp.handlers").setup()
 require "user.lsp.null-ls"
 require 'lspconfig'.volar.setup {
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' },
+  init_options = {
+    typescript = {
+      serverPath = '/home/jens/.npm-global/lib/node_modules/typescript/lib/tsserverlibrary.js'
+      -- Alternative location if installed as root:
+      -- serverPath = '/usr/local/lib/node_modules/typescript/lib/tsserverlibrary.js'
+    }
+  },
   settings = {
     css = {
       lint = {
@@ -18,7 +26,7 @@ require 'lspconfig'.volar.setup {
 require 'lspconfig'.tailwindcss.setup {}
 require 'lspconfig'.cssls.setup {
   cmd = { "vscode-css-language-server", "--stdio" },
-  filetype = { "css", "scss", "less", "vue"},
+  filetype = { "css", "scss", "less", "vue" },
   settings = {
     css = {
       lint = {
