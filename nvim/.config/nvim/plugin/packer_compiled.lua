12a09d3ca1568f58babdcd2d56551dc9201dc9a1
jens3911 <jens3911@live.de>
2022-12-17 15:18:16 +0100

    changed colors in tmux and i3

--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -200,11 +200,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/jens/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
-  },
-  ["zen-mode.nvim"] = {
-    loaded = true,
-    path = "/home/jens/.local/share/nvim/site/pack/packer/start/zen-mode.nvim",
-    url = "https://github.com/folke/zen-mode.nvim"
   }
 }
 
